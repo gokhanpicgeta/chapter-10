834d38015fc1966f48cb50a387ece20b2dccc28d
gokhanpicgeta <vishnuprasad97@hotmail.com>
2024-10-09 20:23:16 +1100

    fixed lined endings in build image.sh

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -36,7 +36,7 @@ async function main() {
     //
     app.get("/video", async (req, res) => {
 
-        console.log('hello world');
+        console.log('hello worlds');
         const videoId = req.query.id;
 
         // Retreives the data from the metadata microservice.
