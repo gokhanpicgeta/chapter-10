13eb0056ace2e2df538f6e56da10ad7474410aff
gokhanpicgeta <vishnuprasad97@hotmail.com>
2024-10-09 20:33:45 +1100

    Two exclamations

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -36,7 +36,7 @@ async function main() {
     //
     app.get("/video", async (req, res) => {
 
-        console.log('hello world!');
+        console.log('hello world!!');
         const videoId = req.query.id;
 
         // Retreives the data from the metadata microservice.
